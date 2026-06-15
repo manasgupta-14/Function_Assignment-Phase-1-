e45c4309669ffa920cdd6ec0f3452adde9dc6cb1
Manas Gupta <manasmggupta@gmail.com>
2026-06-15 17:37:18 +0530

    Js/Find_GCD.js is Added

---
+++ Js/Find_GCD.js
@@ -0,0 +1,13 @@
+function findGCD(a,b){
+    let gcd=1;
+
+    for(let i=1;i<Math.min(a,b);i++){
+        if(a%i===0 && b%i===0){
+            gcd=i;
+        }
+    }
+
+    console.log(gcd);
+}
+
+findGCD(12,18);
