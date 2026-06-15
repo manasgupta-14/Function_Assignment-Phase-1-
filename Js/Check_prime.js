a316c086ae32ff0665a865b19300215ddfd28c04
Manas Gupta <manasmggupta@gmail.com>
2026-06-15 18:09:15 +0530

    Js/Check_prime.js

---
+++ Js/Check_prime.js
@@ -0,0 +1,21 @@
+function checkPrime(num){
+    let count=0;
+
+    if(num<1){
+        return false;
+    }
+    
+    for(let i=1;i<=num;i++){
+        if(num%i===0){
+            count++;
+        }
+    }
+
+    if(count===2){
+        console.log(num+" is Prime Number");
+    }else{
+        console.log(num+" is not Prime Number")
+    }
+}
+
+checkPrime(7);
